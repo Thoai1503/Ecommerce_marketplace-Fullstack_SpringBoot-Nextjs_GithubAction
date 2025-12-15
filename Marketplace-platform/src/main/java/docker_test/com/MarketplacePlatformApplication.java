14a357d6d4e3ca2e50a21b2df6370ac891bee76b
Đặng Văn Thành Điệp <dangvanthanhdiep2711@gmail.com>
2025-12-15 18:28:34 +0700

    nhánh test

--- Marketplace-platform/src/main/java/docker_test/com/MarketplacePlatformApplication.java
+++ Marketplace-platform/src/main/java/docker_test/com/MarketplacePlatformApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
+@ComponentScan
 public class MarketplacePlatformApplication {
 
 	public static void main(String[] args) {
