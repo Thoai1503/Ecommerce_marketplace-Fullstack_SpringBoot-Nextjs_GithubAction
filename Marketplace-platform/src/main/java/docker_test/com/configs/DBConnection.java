d2221778930d2e08162c526b4e2d974d5282c763
Thoai1503 <vothoai1503@gmail.com>
2025-12-19 17:14:02 +0700

    Modify db config

--- Marketplace-platform/src/main/java/docker_test/com/configs/DBConnection.java
+++ Marketplace-platform/src/main/java/docker_test/com/configs/DBConnection.java
@@ -1,34 +1,55 @@
 package docker_test.com.configs;
 
+import java.io.InputStream;
 import java.sql.Connection;
+import java.util.Properties;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
-import docker_test.com.utils.StringValue;
-
-
 public final class DBConnection {
-		private static DBConnection instance=null;
-		private DBConnection() {}
-		public static DBConnection GetInstance() {
-			if (instance==null) {
-				instance=new DBConnection();
-			}
-			return instance;
-		}
-		public Connection GetConn() {
-			try {
-				MysqlDataSource ds = new MysqlDataSource();
-		        ds.setUser(StringValue.DATABASE_MySQL);
-		        ds.setPassword(StringValue.PWD_MySQL);
-		        ds.setServerName(StringValue.MySQL_Sever);
-		        ds.setPortNumber(Integer.parseInt(StringValue.PORT_MySQL));
-		        ds.setDatabaseName(StringValue.DATABASE_MySQL);
-		        return ds.getConnection();
-			} catch (Exception e) {
-				// TODO: handle exception
-				System.out.println(e.getMessage());
-			}
-			return null;	
-		}
+
+    private static DBConnection instance;
+    private static final Properties props = new Properties();
+
+    // Load config 1 lần duy nhất
+    static {
+        try (InputStream input = DBConnection.class
+                .getClassLoader()
+                .getResourceAsStream("application.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("❌ Không tìm thấy application.properties");
+            }
+            System.out.print("Find application.properties");
+            props.load(input);
+        } catch (Exception e) {
+            throw new RuntimeException("❌ Lỗi load DB config", e);
+        }
+    }
+
+    private DBConnection() {}
+
+    public static DBConnection getInstance() {
+        if (instance == null) {
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+
+    public static Connection getConn() {
+        try {
+            MysqlDataSource ds = new MysqlDataSource();
+            ds.setServerName(props.getProperty("mysql.host"));
+            ds.setPortNumber(Integer.parseInt(props.getProperty("mysql.port")));
+            ds.setDatabaseName(props.getProperty("mysql.database"));
+            ds.setUser(props.getProperty("mysql.username"));
+            ds.setPassword(props.getProperty("mysql.password"));
+            ds.setUseSSL(false);
+            System.out.print("Conneting ..");
+            return ds.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
