a66444f28f9125f9098c9e381c3329420f00613c
Thoai1503 <vothoai1503@gmail.com>
2025-12-30 00:30:37 +0700

    update

--- Marketplace-platform/src/main/java/docker_test/com/models/attribute/Attribute.java
+++ Marketplace-platform/src/main/java/docker_test/com/models/attribute/Attribute.java
@@ -1,61 +1,40 @@
 package docker_test.com.models.attribute;
 
-public final class Attribute {
-	private int id;
-	private String name;
-	private String slug;
-	private int dataType;
-	private int status;
-
-	public Attribute() {
-		this.status = 1;
-	}
-
-	public Attribute(int id, String name, String slug, int dataType, int status) {
+public class Attribute {
+      private Integer id;
+      public Attribute(Integer id, String name, String slug, Integer data_type) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.slug = slug;
-		this.dataType = dataType;
-		this.status = status;
+		this.data_type = data_type;
 	}
-
-	public int getId() {
+      public Attribute() {}
+	  public Integer getId() {
 		return id;
 	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSlug() {
-		return slug;
-	}
-
-	public void setSlug(String slug) {
-		this.slug = slug;
-	}
-
-	public int getDataType() {
-		return dataType;
-	}
-
-	public void setDataType(int dataType) {
-		this.dataType = dataType;
-	}
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
+	  public void setId(Integer id) {
+		  this.id = id;
+	  }
+	  public String getName() {
+		  return name;
+	  }
+	  public void setName(String name) {
+		  this.name = name;
+	  }
+	  public String getSlug() {
+		  return slug;
+	  }
+	  public void setSlug(String slug) {
+		  this.slug = slug;
+	  }
+	  public Integer getData_type() {
+		  return data_type;
+	  }
+	  public void setData_type(Integer data_type) {
+		  this.data_type = data_type;
+	  }
+	  private String name;
+      private String slug;
+      private Integer data_type;
 }
