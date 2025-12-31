80aa3ffde72ed505916fb90f1c2b9d0cb4a3b0b6
Thoai1503 <vothoai1503@gmail.com>
2025-12-31 20:14:39 +0700

    update attribute values array for category-attribute by category_id

--- marketfrontend/src/app/admin/category-attribute/page.tsx
+++ marketfrontend/src/app/admin/category-attribute/page.tsx
@@ -3,7 +3,18 @@ import React, { Suspense } from "react";
 
 const page = () => {
   return (
-    <Suspense fallback={<div>Loading category attributes...</div>}>
+    <Suspense
+      fallback={
+        <div
+          className="d-flex justify-content-center align-items-center"
+          style={{ minHeight: "100vh" }}
+        >
+          <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      }
+    >
       <CategoryAttributeManager />
     </Suspense>
   );
