597ed5a261cdb462b7e6dc955f00c170ee68bdba
Thoai1503 <vothoai1503@gmail.com>
2026-02-08 22:45:31 +0700

    config cors origin

--- marketfrontend/src/helper/api.ts
+++ marketfrontend/src/helper/api.ts
@@ -1,7 +1,6 @@
 //export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const API_URL = "https://ezonex.duckdns.org";
 // export const API_URL = "http://103.90.225.130:8000";
-
+export const API_URL = "https://ezonex.duckdns.org";
 export const INTERNAL_API = "http://backend:8080";
 // export const INTERNAL_API = "http://localhost:8000";
 // export const API_URL = "http://localhost:8000";
