14fee8a255020703f8cbb7a4bd3df1611f86d87a
Thoai1503 <vothoai1503@gmail.com>
2026-02-20 19:11:11 +0700

    upgade microservice structure + api gateway

--- Marketplace-platform/src/main/java/docker_test/com/MarketplacePlatformApplication.java
+++ Marketplace-platform/src/main/java/docker_test/com/MarketplacePlatformApplication.java
@@ -3,11 +3,13 @@ package docker_test.com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 
 @SpringBootApplication
 @ComponentScan
+
 public class MarketplacePlatformApplication {
 
 	
