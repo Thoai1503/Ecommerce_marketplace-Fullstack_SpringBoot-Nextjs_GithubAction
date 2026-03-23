20dbece8665d5d9edaddd2d4d5878b9d08528b89
Thoai1503 <vothoai1503@gmail.com>
2026-03-23 22:32:08 +0700

    rebuild

--- Marketplace-platform/src/main/java/docker_test/com/configs/CorsConfig.java
+++ Marketplace-platform/src/main/java/docker_test/com/configs/CorsConfig.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //package docker_test.com.configs;
 //
 //import org.springframework.context.annotation.Configuration;
@@ -24,30 +23,3 @@
 //                .maxAge(3600);
 //    }
 //}
-=======
-package docker_test.com.configs;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // áp dụng cho toàn bộ API
-                .allowedOrigins(
-                        "http://localhost:3000",
-                         "http://localhost:3002",
-                        "http://localhost:5173",
-                        "http://103.90.225.130:4000",
-                        "http://103.90.225.130"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
-}
->>>>>>> c0a1f7cc9a8518c42e84670ceef22ecdc13e67da
