ecbfc33ff1dcebf24951227bfae8b4ecd808fbfa
Thoai1503 <vothoai1503@gmail.com>
2026-03-31 07:31:16 +0700

    Implement database connection utility and configure environment-based properties for microservices

--- marketfrontend/src/helper/api.ts
+++ marketfrontend/src/helper/api.ts
@@ -1,11 +1,10 @@
 //export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const API_URL = "http://103.90.225.130:8000";
-export const INTERNAL_API = "http://backend:8080";
-// export const INTERNAL_API = "http://localhost:8000";
-// export const API_URL = "http://localhost:8000";
+// export const API_URL = "http://103.90.225.130:8000";
+// export const INTERNAL_API = "http://backend:8080";
+//export const INTERNAL_API = process.env.INTERNAL_API;
+export const INTERNAL_API = process.env.INTERNAL_API;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 //f/vscvsdvsed
-export const PROVINCE_API =
-  "https://dev-online-gateway.ghn.vn/shiip/public-api/master-data";
-
-export const ADDRESS_KEY = process.env.ADDRESS_KEY;
+export const PROVINCE_API = process.env.NEXT_PUBLIC_PROVINCE_API;
+export const ADDRESS_KEY = process.env.NEXT_PUBLIC_ADDRESS_KEY;
