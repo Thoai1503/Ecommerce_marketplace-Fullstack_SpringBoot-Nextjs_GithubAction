298bc6e32c20e5f5ba5417d11f38951dd5fc759a
Thoại <vothoai1503@gmail.com>
2026-04-22 21:17:41 +0700

    fix: Add comment to StockServiceApplication for CI/CD pipeline clarity

--- stock-service/src/main/java/stock_gateway/com/StockServiceApplication.java
+++ stock-service/src/main/java/stock_gateway/com/StockServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StockServiceApplication {
 
+
+	// Change to trigger build and test in CI/CD pipeline
 	public static void main(String[] args) {
 		SpringApplication.run(StockServiceApplication.class, args);
 	}
